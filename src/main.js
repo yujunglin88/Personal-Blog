d5d199f1d2646338a37a5220df6cc58faae33601
Jeff Lin <yujunglin88@gmail.com>
2022-06-10 18:10:42 +1200

    initial setup

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const text = document.querySelector(".title");
+const button = document.querySelector('#button');
+
+text.style.backgroundColor = 'rgba(255,255,255,)'
+text.style.color = 'rgba(0,0,0,)'
