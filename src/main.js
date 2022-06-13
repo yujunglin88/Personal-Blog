7b92db0401c91e8fb3122279708f1e695160a4c2
Jeff Lin <yujunglin88@gmail.com>
2022-06-14 11:45:01 +1200

    attemp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const text = document.querySelector(".title");
 const button = document.querySelector('#button');
 
-text.style.backgroundColor = 'rgba(255,255,255,)'
-text.style.color = 'rgba(0,0,0,)'
+console.log("pre");
+
+text.style.backgroundColor ='rgba(255,0,0,)';
+text.style.color = 'rgba(0,0,0,)';
+
+console.log("post");
