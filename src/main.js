05b31101e5159b3aabe1c1f7c82d93259ccb713c
Jeff <yujunglin88@gmail.com>
2023-09-08 16:52:18 +1200

    init v2

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const text = document.querySelector(".title");
-const button = document.querySelector('#button');
-
-console.log("pre");
-
-text.style.backgroundColor ='rgba(255,0,0,)';
-text.style.color = 'rgba(0,0,0,)';
-
-console.log("post");
