0d7bbab9be60996a1c9a52522e6a916ffd170196
Jeff <yujunglin88@gmail.com>
2023-09-10 12:57:21 +1200

    Update main.js
    * working highlighting
    * move created armillary sphere to a function

--- personal-blog/main.js
+++ personal-blog/main.js
@@ -18,63 +18,74 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setZ(30)
 
+// postprocessing
 const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene, camera);
-
 composer.addPass(renderPass);
 
-
+// ourline effect
 const outline = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, camera);
-outline.edgeThickness = 10.0;
-outline.edgeStrength = 5.0;
+outline.edgeThickness = 4.0;
+outline.edgeStrength = 8.0;
+outline.edgeGlow = 0.5;
 outline.visibleEdgeColor.set(0x4c9aff);
-
+outline.hiddenEdgeColor.set(0x4c9aff);
 composer.addPass(outline);
 
+// anti-aliasing
 const fxaaShader = new ShaderPass(FXAAShader);
 fxaaShader.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
 composer.addPass(fxaaShader);
 
-// Background
-const spaceTexture = new THREE.TextureLoader().load('res/space.jpg')
-// scene.background = spaceTexture
+// Torus
+function create_torus(radius, tube, radialSegments, tubularSegments, color, x, y, z, name=''){
+  const geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments)
+  const material = new THREE.MeshStandardMaterial({ color: color})
+  const torus = new THREE.Mesh(geometry, material)
+  torus.name = name
+  torus.position.set(x, y, z)
+  return torus
+}
 
+// create a armillary sphere with a dice inside
+function create_armillary_sphere(name, x, y, z, images=[]){
+  // 6 sided dice with with 6 different images
+  const loader = new THREE.TextureLoader()
+  const matx = images.map(path => new THREE.MeshBasicMaterial({ map: loader.load(path) }))
+  const dice = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), matx)
+  dice.position.set(x, y, z)
+  // create 3 toruses
+  const toruses = [
+    create_torus(5, 0.1, 16, 200, 0xff12ef, x, y, z),
+    create_torus(5.2, 0.1, 16, 200, 0xffdaef, x, y, z),
+    create_torus(5.4, 0.1, 16, 200, 0xb612ff, x, y, z)
+  ]
+  // attach a invisible sphere to the armillary sphere as its outer boundary
+  const outerSphereGeo = new THREE.SphereGeometry(5.5, 100, 100)
+  const outerSphereMaterial = new THREE.MeshBasicMaterial({ color: 0x4c9aff, transparent: true, opacity: 0.0 })
+  const outerSphere = new THREE.Mesh(outerSphereGeo, outerSphereMaterial)
+  outerSphere.name = name
+  outerSphere.position.set(x, y, z)
+  // attach a light source to the center of the armillary sphere
+  const sphereLight = new THREE.PointLight(0xa3c8f7, 100)
+  sphereLight.position.set(x, y, z)
+  sphereLight.visible = false
 
-// 6 sided dice with with 6 different images
-const loader = new THREE.TextureLoader()
-const matx = [
+  // add all the objects to the scene
+  scene.add(dice)
+  toruses.forEach(torus => scene.add(torus))
+  scene.add(outerSphere)
+  scene.add(sphereLight)
+  return [toruses, dice, sphereLight]
+}
+const [toruses, dice, sphereLight] = create_armillary_sphere("pups", 0, 0, 0, [
   '/res/pups/IMG-0167.jpg',
   '/res/pups/IMG-1102.jpg',
   '/res/pups/IMG-1684.jpg',
   '/res/pups/IMG-1690.jpg',
   '/res/pups/IMG-2108.jpg',
   '/res/pups/IMG-2111.jpg'
-].map(path => new THREE.MeshBasicMaterial({ map: loader.load(path) }))
-const dice = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), matx)
-dice.position.set(0, 0, 0)
-scene.add(dice)
-
-
-// Torus
-function create_torus(radius, tube, radialSegments, tubularSegments, color, x, y, z){
-  const geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments)
-  const material = new THREE.MeshStandardMaterial({ color: color})
-  const torus = new THREE.Mesh(geometry, material)
-  torus.position.set(x, y, z)
-  return torus
-}
-const toruses = [
-  create_torus(5, 0.1, 16, 200, 0xff12ef, 0, 0, 0), 
-  create_torus(5.2, 0.1, 16, 200, 0xffdaef, 0, 0, 0), 
-  create_torus(5.4, 0.1, 16, 200, 0xb612ff, 0, 0, 0) 
-]
-toruses.forEach(torus => scene.add(torus))
-// attach a invisible sphere to the torus to make it glow
-const outerSphereGeo = new THREE.SphereGeometry(5.5, 100, 200)
-const outerSphereMaterial = new THREE.MeshBasicMaterial({ color: 0x4c9aff, transparent: true, opacity: 0.0 })
-const outerSphere = new THREE.Mesh(outerSphereGeo, outerSphereMaterial)
-outerSphere.name = "outerSphere"
-scene.add(outerSphere)
+])
 
 // Stars
 function addStar(){
@@ -94,23 +105,17 @@ for(let i = 0; i < 200; i++){
 
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff, 100)
-pointLight.position.set(0, 15, 0)
+const mainPointLight = new THREE.PointLight(0xffffff, 100)
+mainPointLight.position.set(0, 15, 0)
 
 // soft ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-scene.add(pointLight, ambientLight)
-// scene.add(pointLight)
-
-const sphereLight = new THREE.PointLight(0xffffff, 100)
-sphereLight.position.set(0, 0, 0)
-scene.add(sphereLight)
-
+scene.add(mainPointLight, ambientLight)
 
 // light helper
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
-scene.add(lightHelper, gridHelper)
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50)
+// scene.add(lightHelper, gridHelper)
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -131,9 +136,7 @@ let intersected;
 const selectedObjects = [];
 
 function addSelectedObjects(object){
-  if (selectedObjects.length > 0) {
-    selectedObjects.pop();
-  }
+  selectedObjects.pop();
   selectedObjects.push(object);
 }
 
@@ -142,27 +145,24 @@ function intersection(){
   const intersects = raycaster.intersectObjects(scene.children, false);
 
   if ( intersects.length > 0 ) {
+    // console.log(intersects[0].object)
     if ( intersected != intersects[0].object && intersects[0].object.type === "Mesh" ) {
-
       
       intersected = intersects[0].object;
 
-      console.log(intersected)
-      console.log(intersected.name)
+      // console.log(intersected)
+      // console.log(intersected.name)
 
-      addSelectedObjects(intersected);
+      addSelectedObjects(dice)
       outline.selectedObjects = selectedObjects;
-      if (intersected.name == "outerSphere") {
-        console.log("outerSpherelight")
-        sphereLight.power = 100
-      } 
+
+      // turn on the light
+      sphereLight.visible = true
     }
   } else {
     intersected = null;
-    if (selectedObjects.length > 0) {
-      selectedObjects.pop();
-    }
-    sphereLight.power = 0
+    sphereLight.visible = false
+    selectedObjects.pop();
   }
 }
 
